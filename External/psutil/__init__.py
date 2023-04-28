94854e3119af49a64a4c28a630c80b4bf6c4dcf7
karrycharon <karrycharon@qq.com>
2023-04-28 10:02:33 +0800

    add psutil linux x86-64

--- External/psutil/__init__.py
+++ External/psutil/__init__.py
@@ -1,7 +1,11 @@
-import sys
-if sys.platform == "win32":
+import platform
+if platform.sys.platform == "win32":
     from . import psutil_win as psutil
-elif sys.platform == "arm64":
-    from . import psutil_macosx_arm64 as psutil
+elif platform.sys.platform == "darwin":
+    import platform
+    if platform.machine() == "arm64":
+        from . import psutil_macosx_arm64 as psutil
+    else:
+        from . import psutil_macosx_x86_64 as psutil
 else:
-    from . import psutil_macosx_arm64 as psutil
+    from . import psutil_linux_x86_64 as psutil
