95319048e145f93624d030055cbd3005d1964dad
karrycharon <karrycharon@qq.com>
2023-05-15 14:18:14 +0800

    add reload gui reg; add multi window gui reg;

--- MultiLineText/integration.py
+++ MultiLineText/integration.py
@@ -11,7 +11,7 @@ from math import ceil
 from pathlib import Path
 from bpy.types import SpaceNodeEditor
 from gpu_extras.batch import batch_for_shader
-from ..utils import _T
+from ..utils import _T, logger
 from ..SDNode.tree import TREE_TYPE
 try:
     import imgui
@@ -272,7 +272,7 @@ class GlobalImgui:
     _instance = None
     imgui_ctx = None
     draw_handlers = {}
-    callbacks = set()
+    callbacks = {}
     imgui_backend = None
 
     def __new__(cls):
@@ -298,29 +298,34 @@ class GlobalImgui:
         self.setup_key_map()
 
     def shutdown_imgui(self):
-        for SpaceType, draw_handler in self.draw_handlers.items():
-            SpaceType.draw_handler_remove(draw_handler, 'WINDOW')
+        for (space, area), draw_handler in self.draw_handlers.items():
+            space.draw_handler_remove(draw_handler, "WINDOW")
         self.draw_handlers.clear()
-        # imgui.destroy_context(self.imgui_ctx)
-        # self.imgui_ctx = None
+        imgui.destroy_context(self.imgui_ctx)
+        self.imgui_ctx = None
 
     def handler_add(self, callback):
         self.init_imgui()
         space, area = bpy.context.space_data.__class__, bpy.context.area
         if (space, area) not in self.draw_handlers:
-            self.draw_handlers[(space, area)] = space.draw_handler_add(self.draw, (space, area), 'WINDOW', 'POST_PIXEL')
-        self.callbacks.add(callback)
+            self.draw_handlers[(space, area)] = space.draw_handler_add(self.draw, (area,), "WINDOW", "POST_PIXEL")
+        if area not in self.callbacks:
+            self.callbacks[area] = {callback}
+        else:
+            self.callbacks[area].add(callback)
 
     def handler_remove(self, handle=None):
+        # clear all
         if not handle:
             self.callbacks.clear()
             self.shutdown_imgui()
-        elif handle in self.callbacks:
-            for (space, area) in self.draw_handlers:
-                if area != bpy.context.area:
-                    continue
-                self.draw_handlers.pop((space, area))
-            self.callbacks.discard(handle)
+            return
+        # clear handle only
+        for (space, area) in self.draw_handlers:
+            if area != bpy.context.area:
+                continue
+            self.draw_handlers.pop((space, area))
+            self.callbacks[area].discard(handle)
 
     def apply_ui_settings(self):
         region = bpy.context.region
@@ -332,7 +337,7 @@ class GlobalImgui:
         style.frame_border_size = 1
         # io.font_global_scale = context.preferences.view.ui_scale
 
-    def draw(self, space, area):
+    def draw(self, area):
         if area != bpy.context.area:
             return
 
@@ -343,9 +348,8 @@ class GlobalImgui:
         frame_bg_color = (0.512, 0.494, 0.777, 0.573)
         imgui.push_style_color(imgui.COLOR_TITLE_BACKGROUND_ACTIVE, *title_bg_active_color)
         imgui.push_style_color(imgui.COLOR_FRAME_BACKGROUND, *frame_bg_color)
-        for cb in self.callbacks:
-            if bpy.context.space_data.__class__ == space:
-                cb(bpy.context)
+        for cb in self.callbacks[area]:
+            cb(bpy.context)
         imgui.pop_style_color()
         imgui.pop_style_color()
         imgui.end_frame()
@@ -388,15 +392,7 @@ def inbox(x, y, w, h, mpos):
     return False
 
 
-@lru_cache
-def get_wrap_text(text, lwidth):
-    text = text.replace("\n", "")
-    return "\n".join(text[i * lwidth: (i + 1) * lwidth] for i in range(ceil(len(text) / lwidth)))
-
-
-class MLTOps(bpy.types.Operator):
-    bl_idname = "sdn.multiline_text"
-    bl_label = "MLT"
+class BaseDrawCall:
 
     key_map = {
         'TAB': imgui.KEY_TAB,
@@ -428,43 +424,25 @@ class MLTOps(bpy.types.Operator):
         'RIGHT_SHIFT': 128 + 6,
         'OSKEY': 128 + 7,
     }
-    REG = False
+    REG_AREA = set()
+        
+    def try_reg(self, area: bpy.types.Area):
+        if area in self.__class__.REG_AREA:
+            return
+        self.__class__.REG_AREA.add(area)
+        GlobalImgui().handler_add(self.draw_call)
+        return True
 
-    def invoke(self, context, event):
-        if MLTOps.REG:
-            return {"FINISHED"}
-        self.mpos = (0, 0)
-        self.cover = False
-        GlobalImgui().handler_add(self.draw)
-        MLTOps.REG = True
-        self.io = imgui.get_io()
-        context.window_manager.modal_handler_add(self)
-        return {'RUNNING_MODAL'}
-
-    def modal(self, context, event):
-        if not context.area:
-            self.clear()
-            return {'CANCELLED'}
-        self.mpos = event.mouse_region_x, event.mouse_region_y
-        context.area.tag_redraw()
-
-        # if event.type in {'RIGHTMOUSE', 'ESC'}:
-        #     self.shutdown()
-        #     return {'CANCELLED'}
-
-        if not self.cover:
-            return {"PASS_THROUGH"}
-        self.modal_imgui(context, event)
-        return {"RUNNING_MODAL"}
+    def draw_call(self, context: bpy.types.Context):
+        imgui.show_test_window()
 
     def clear(self):
-        GlobalImgui().handler_remove(self.draw)
-        MLTOps.REG = False
+        GlobalImgui().handler_remove(self.draw_call)
 
-    def modal_imgui(self, context, event):
+    def poll_events(self, context: bpy.types.Context, event: bpy.types.Event):
         region = context.region
-        io = self.io
-
+        io = imgui.get_io()
+        
         io.mouse_pos = (event.mouse_region_x, region.height - 1 - event.mouse_region_y)
 
         if event.type == 'LEFTMOUSE':
@@ -481,8 +459,6 @@ class MLTOps(bpy.types.Operator):
 
         elif event.type == 'WHEELDOWNMOUSE':
             io.mouse_wheel = -1
-
-        # print(f"Event type={event.type}, unicode={event.unicode}")
 
         if event.type in self.key_map:
             if event.value == 'PRESS':
@@ -510,6 +486,52 @@ class MLTOps(bpy.types.Operator):
         if event.unicode and 0 < (char := ord(event.unicode)) < 0x10000:
             io.add_input_character(char)
 
+
+@lru_cache
+def get_wrap_text(text, lwidth):
+    text = text.replace("\n", "")
+    return "\n".join(text[i * lwidth: (i + 1) * lwidth] for i in range(ceil(len(text) / lwidth)))
+
+
+class MLTOps(bpy.types.Operator, BaseDrawCall):
+    bl_idname = "sdn.multiline_text"
+    bl_label = "MLT"
+
+    def invoke(self, context, event):
+        self.area = context.area
+        if not self.try_reg(self.area):
+            return {"FINISHED"}
+        self.mpos = (0, 0)
+        self.cover = False
+        self.io = imgui.get_io()
+        context.window_manager.modal_handler_add(self)
+        return {'RUNNING_MODAL'}
+
+    def modal(self, context, event):
+        if not context.area:
+            self.clear()
+            return {"CANCELLED"}
+        self.mpos = event.mouse_region_x, event.mouse_region_y
+        w, h = context.region.width, context.region.height
+        x = context.region.x
+        in_area = x < self.mpos[0] < x + w and 0 < self.mpos[1] < h
+        if not in_area:  # multi windows cause event miss
+            return {"PASS_THROUGH"}
+        context.area.tag_redraw()
+
+        # if event.type in {'RIGHTMOUSE', 'ESC'}:
+        #     self.shutdown()
+        #     return {'CANCELLED'}
+
+        if not self.cover:
+            return {"PASS_THROUGH"}
+        self.poll_events(context, event)
+        return {"RUNNING_MODAL"}
+
+    def clear(self):
+        super().clear()
+        self.__class__.REG_AREA.discard(self.area)
+
     def can_draw(self):
         tree = bpy.context.space_data.edit_tree
         if not tree or bpy.context.space_data.tree_type != TREE_TYPE:
@@ -519,30 +541,19 @@ class MLTOps(bpy.types.Operator):
             return
         return node
 
-    def draw(self, context: bpy.types.Context):
+    def draw_call(self, context: bpy.types.Context):
         if not (node := self.can_draw()):
             return
         flags = imgui.WINDOW_NO_COLLAPSE | imgui.WINDOW_NO_BRING_TO_FRONT_ON_FOCUS | imgui.WINDOW_NO_SAVED_SETTINGS
-        imgui.begin(_T(" Prompts"), closable=False, flags=flags)
+        imgui.begin(_T(" Prompts") + "##" + hex(hash(context.area)), closable=False, flags=flags)
         imgui.set_window_position(50, 20, condition=imgui.ONCE)
         imgui.set_window_size(300, 300, condition=imgui.ONCE)
-        # imgui.core.set_window_position(300, 300)
-        # imgui.core.set_window_size(300, 300)
-        # char_width = imgui.core.calc_text_size("W").x
+
         w, h = imgui.core.get_window_size()
         lnum = max(1, int(w * 2 // imgui.get_font_size()) - 3)
-        # if self.io.mouse_down[2]: # middle mouse
-        #     imgui.WIN_POS = imgui.core.get_window_position()
-        #     imgui.WIN_MOVED = True
 
         def cb(data):
             p = data.cursor_pos
-            # print(imgui.core.get_clipboard_text())
-            # io.display_fb_scale = (2, 2)
-            # if io.keys_down[self.key_map["SPACE"]] and p == len(data.buffer):
-            #     return
-            # if io.keys_down[self.key_map["RET"]]:
-            #     return
             if data.event_flag == imgui.INPUT_TEXT_CALLBACK_EDIT:
                 node.text = data.buffer.replace("\n", "")
                 text = get_wrap_text(data.buffer, lnum)
@@ -550,9 +561,7 @@ class MLTOps(bpy.types.Operator):
                 data.insert_chars(0, text)
                 data.buffer_dirty = True
                 backspace = self.io.keys_down[self.key_map['BACK_SPACE']]
-                # print(p, len(data.buffer), (p - 1) % (lnum+1), p%(lnum+1), lnum)
-                # if p < len(data.buffer) and data.buffer[p] == "\n":
-                #     p += 1
+
                 if not backspace and p % (lnum + 1) == 0:
                     p += 1
                 data.cursor_pos = min(max(0, p), data.buffer_size)
@@ -569,14 +578,57 @@ class MLTOps(bpy.types.Operator):
         x, y = imgui.core.get_window_position()
         y = context.region.height - 1 - y
         self.cover = inbox(x, y, w, h, self.mpos)
-        self.io.keys_down[self.key_map["ESC"]] = not self.cover
+        cx, cy = imgui.core.get_window_content_region_min()
+        cw, ch = imgui.core.get_window_content_region_max()
+        in_content = inbox(x + cx, y - cy, w + cx, ch - cy - 30, self.mpos)
+        if in_content:
+            imgui.core.set_keyboard_focus_here(-1)
+        self.io.keys_down[self.key_map["ESC"]] = not in_content
+
         imgui.end()
+
+
+class GuiTest(bpy.types.Operator, BaseDrawCall):
+    bl_idname = "sdn.gui_test"
+    bl_label = "Gui Test"
+    
+    REG_AREA = set()
+    
+    def invoke(self, context, event):
+        self.area = context.area
+        if not self.try_reg(self.area):
+            return {"FINISHED"}
+        context.window_manager.modal_handler_add(self)
+        return {'RUNNING_MODAL'}
+
+    def modal(self, context, event):
+        if not context.area:
+            self.clear()
+            return {"CANCELLED"}
+        self.mpos = event.mouse_region_x, event.mouse_region_y
+        w, h = context.region.width, context.region.height
+        x = context.region.x
+        in_area = x < self.mpos[0] < x + w and 0 < self.mpos[1] < h
+        if not in_area:  # multi windows cause event miss
+            return {"PASS_THROUGH"}
+        context.area.tag_redraw()
+        self.poll_events(context, event)
+        return {"RUNNING_MODAL"}
+
+
+@bpy.app.handlers.persistent
+def reload(scene):
+    BaseDrawCall.REG_AREA.clear()
 
 
 def multiline_register():
     bpy.utils.register_class(MLTOps)
+    bpy.utils.register_class(GuiTest)
+    
+    bpy.app.handlers.load_pre.append(reload)
 
 
 def multiline_unregister():
     GlobalImgui().handler_remove()
     bpy.utils.unregister_class(MLTOps)
+    bpy.utils.unregister_class(GuiTest)
