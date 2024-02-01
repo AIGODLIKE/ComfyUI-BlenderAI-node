c1bf32762e366c2703156ab94fb025b38f40c23a
karrycharon <karrycharon@qq.com>
2024-02-01 13:46:43 +0800

    format SDNode

--- SDNode/history/__init__.py
+++ SDNode/history/__init__.py
@@ -3,7 +3,7 @@ import bpy
 from copy import deepcopy
 from datetime import datetime
 from pathlib import Path
-
+from ...utils import read_json
 
 class History:
     path = Path(__file__).parent.joinpath("history.json")
@@ -11,30 +11,33 @@ class History:
     is_dirty = True
     cache_histories = []
 
+    @staticmethod
     def put_history(history):
         try:
             if not history:
                 return
             history = {"history": history, "name": datetime.now().strftime("%Y-%m-%d %H:%M:%S")}
             if History.path.exists():
-                histories = json.loads(History.path.read_text())
+                histories = read_json(History.path)
             else:
                 histories = []
             histories.append(history)
             # 限制历史记录数量
             histories = histories[-History.num:]
-            History.path.write_text(json.dumps(histories, indent=4, ensure_ascii=False))
+            History.path.write_text(json.dumps(histories, indent=4, ensure_ascii=False), encoding="utf8")
             History.is_dirty = True
         finally:
             ...
 
+    @staticmethod
     def get_history():
         if History.is_dirty and History.path.exists():
-            History.cache_histories = json.loads(History.path.read_text())
+            History.cache_histories = read_json(History.path)
             History.cache_histories.reverse()
             History.is_dirty = False
         return History.cache_histories
 
+    @staticmethod
     def get_history_by_name(name):
         for i in History.get_history():
             if i["name"] != name:
@@ -42,6 +45,7 @@ class History:
             return deepcopy(i["history"])
         return None
 
+    @staticmethod
     def update_timer():
         try:
             bpy.context.scene.sdn_history_item.clear()
@@ -52,5 +56,6 @@ class History:
             print("Update History Error: ", e)
         return 1
 
+    @staticmethod
     def register_timer():
         bpy.app.timers.register(History.update_timer, persistent=True)
