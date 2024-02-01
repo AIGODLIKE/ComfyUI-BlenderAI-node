e39625049472eaa59e56d39664e1394497d5a194
karrycharon <karrycharon@qq.com>
2024-02-01 09:30:12 +0800

    add load_json func

--- utils.py
+++ utils.py
@@ -12,6 +12,15 @@ from .timer import Timer
 from .datas import IMG_SUFFIX
 translation = {}
 
+def read_json(path: Path | str) -> dict:
+    import json
+    encodings = ["utf8", "gbk"]
+    for encoding in encodings:
+        try:
+            return json.loads(Path(path).read_text(encoding=encoding))
+        except UnicodeDecodeError:
+            continue
+    return {}
 
 def rmtree(path: Path):
     if path.is_file():
