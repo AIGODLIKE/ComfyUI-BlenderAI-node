c0662fdc5348dbae5a70a718b37ce5312a8eb803
karrcharon <karrycharon@qq.com>
2025-11-28 13:47:09 +0800

    -

--- utils.py
+++ utils.py
@@ -559,7 +559,7 @@ class PkgInstaller:
 
     @staticmethod
     def should_use_user():
-        return platform.system() == "Windows" and bpy.app.binary_path[0].upper() == "C"
+        return platform.system() == "Windows" and Path(bpy.app.binary_path).drive.startswith("C")
 
     @staticmethod
     def try_install(*packages):
