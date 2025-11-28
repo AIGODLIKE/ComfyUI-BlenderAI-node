b10472f3732abbc50aff28409c639fc4d81b60d5
小萌新 working <3209970865@qq.com>
2025-11-28 10:52:14 +0800

    add bpy.types.Object.texture_wh

--- SDNode/products/ViewportLayerRedraw/TextureControl/__init__.py
+++ SDNode/products/ViewportLayerRedraw/TextureControl/__init__.py
@@ -3,6 +3,18 @@ import bpy
 modules = ["gizmo", "texture_space_tool", "ops"]
 
 reg, unreg = bpy.utils.register_submodule_factory(__package__, modules)
+import numpy as np
+
+def get_wh(self):
+    ofl, ofr, oft, ofb = self.texture_space_control_offset[:]
+    ll = np.multiply(ofl, np.divide(iw, dx))
+    rr = np.multiply(ofr, np.divide(iw, dx))
+    tt = np.multiply(oft, np.divide(ih, dy))
+    bb = np.multiply(ofb, np.divide(ih, dy))
+    return Vector((rr + ll, tt + bb))
+
+def set_wh(self, value):
+    ...
 
 
 def register():
@@ -12,6 +24,11 @@ def register():
         default=(0, 0, 0, 0),
         size=4,
         options={"TEXTEDIT_UPDATE"}
+    )
+    bpy.types.Object.texture_wh = bpy.props.IntVectorProperty(
+        name="Texture Width Height", default=(0, 0), size=2,
+        get=get_wh,
+        set=set_wh,
     )
     reg()
 
