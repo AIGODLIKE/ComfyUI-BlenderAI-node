a5fea3f5e70e84b45b4c1c6ca65a7e673783a32c
vuaieo <eaoiu@protonmail.com>
2026-01-06 17:20:31 +0100

    revert the fix because causing other errors.

--- SDNode/blueprints.py
+++ SDNode/blueprints.py
@@ -1306,8 +1306,6 @@ class 预览(BluePrintBase):
             return self.width
         pnum = len(self.prev)
         p0 = self.prev[0].image
-        if not p0:
-            return self.width
         w = max(p0.size[0], p0.size[1])
         if w == 0:
             return self.width
@@ -1385,11 +1383,11 @@ class 预览(BluePrintBase):
                 if not Path(img_path).exists():
                     continue
                 try:
-                    Icon.load_icon(img_path)
-                    if not (img := Icon.find_image(img_path)):
-                        continue
                     p = self.prev.add()
                     # p.image = bpy.data.images.load(img_path)
+                    Icon.load_icon(img_path)
+                    if not (img := Icon.find_image(img_path)):
+                        return
                     p.image = img
                 except TypeError:
                     ...
@@ -1404,8 +1402,6 @@ class PreviewImage(BluePrintBase):
             return self.width
         pnum = len(self.prev)
         p0 = self.prev[0].image
-        if not p0:
-            return self.width
         w = max(p0.size[0], p0.size[1])
         if w == 0:
             return self.width
@@ -1482,11 +1478,11 @@ class PreviewImage(BluePrintBase):
                 if not Path(img_path).exists():
                     continue
                 try:
-                    Icon.load_icon(img_path)
-                    if not (img := Icon.find_image(img_path)):
-                        continue
                     p = self.prev.add()
                     # p.image = bpy.data.images.load(img_path)
+                    Icon.load_icon(img_path)
+                    if not (img := Icon.find_image(img_path)):
+                        return
                     p.image = img
                 except TypeError:
                     ...
@@ -1618,8 +1614,6 @@ class ComfyUIInputs(BluePrintBase):
             return self.width
         pnum = len(self.prev_image)
         p0 = self.prev_image[0].image
-        if not p0:
-            return self.width
         w = max(p0.size[0], p0.size[1])
         if w == 0:
             return self.width
